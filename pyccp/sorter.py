4cf7ce951eb61ef6490f2e69b94d680109fd5810
Alexander Bessman <alexander.bessman@scania.com>
2020-02-26 16:55:21 +0100

    Add new Listener: DAQParser

--- pyccp/sorter.py
+++ pyccp/sorter.py
@@ -25,6 +25,9 @@ __copyright__ = """
 
 import can
 import queue
+import pandas as pd
+from numpy import NaN
+from typing import List
 
 from pyccp import ccp
 
@@ -98,3 +101,67 @@ class CCPMessageSorter(can.Listener):
 
     def get_command_receive_object(self, timeout: float = 0.5):
         return self._cro_queue.get(timeout=timeout)
+
+
+class DAQParser(can.Listener):
+    def __init__(self, dto_id: int,
+                 daq_lists: List[List[ccp.ObjectDescriptorTable]] = [],
+                 enable_logging: bool = False):
+        self.dto_id = dto_id
+        self._queue = queue.Queue()
+        self.logging = enable_logging
+        # Flatten DAQ lists to dict of ODTs
+        # self.odt_dict is used for decoding incoming data
+        self.odt_dict = {}
+        # self.values_dict is used for holding decoded data
+        self.values_dict = {}
+
+        for dl in daq_lists:
+            for odt in dl:
+                self.odt_dict[odt.number] = odt
+
+                for e in odt.elements:
+                    self.values_dict[e.name] = None
+
+        if self.logging:
+            columns = list(self.values_dict.keys())
+            self.log = pd.DataFrame(columns=columns)
+
+    def add_daq_list(self, daq_list: List[ccp.ObjectDescriptorTable]):
+        for odt in daq_list:
+            self.odt_dict[odt.number]: odt
+
+            for e in odt.elements:
+                self.values_dict[e.name] = None
+
+                if self.logging:
+                    self.log.loc[:, e.name] = [NaN] * len(self.log.index)
+
+    def on_message_received(self, msg: can.Message):
+        if msg.arbitration_id == self.dto_id:
+            # The message is a Data Transmission Object
+            pid = msg.data[0]
+
+            if pid < 0xFE:
+                # The DTO is a Data Acquisition Message
+                self._queue.put(msg)
+                odt_number = pid
+                element_values = self.odt_dict[odt_number].decode(msg.data[1:])
+
+                for k, v in element_values.items():
+                    self.values_dict[k] = v
+
+                    if self.logging:
+                        fill = [NaN] * len(self.log.columns)
+                        self.log.loc[pd.to_datime(msg.timestamp, unit="s")] = fill
+                        self.log.loc[pd.to_datime(msg.timestamp, unit="s")][k] = v
+
+    def get(self, element_name: str):
+        self._queue.get(timeout=0.5)
+        return self.values_dict[element_name]
+
+    def get_log(self):
+        if self.logging:
+            return self.log
+        else:
+            return None
